3cb60c234e7ac0f9db360185757c006ad87c70c9
Augusto Flávio Mendonça <augusto.mendonca@mesainc.com.br>
2021-02-25 14:40:27 -0300

    fix: removes unnecessary parameterization in the signin's request and response

--- app/src/main/java/com/augusto/mesanews/cleanarchitecture/data/api/request/SigninRequest.kt
+++ app/src/main/java/com/augusto/mesanews/cleanarchitecture/data/api/request/SigninRequest.kt
@@ -1,9 +1,7 @@
 package com.augusto.mesanews.cleanarchitecture.data.api.request
 
 import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
 data class SigninRequest(
     @Json(name = "email")
     var email: String,
--- app/src/main/java/com/augusto/mesanews/cleanarchitecture/data/api/response/AuthResponse.kt
+++ app/src/main/java/com/augusto/mesanews/cleanarchitecture/data/api/response/AuthResponse.kt
@@ -1,9 +1,7 @@
 package com.augusto.mesanews.cleanarchitecture.data.api.response
 
 import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
 data class AuthResponse(
     @Json(name = "token")
     var token: String,
